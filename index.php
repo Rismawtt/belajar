71ca003a13066e3336b9350af86cf078d8fae8eb
Rismawtt <rismawati.triyani@gmail.com>
2016-04-23 21:30:48 +0700

    tes

---
+++ index.php
@@ -0,0 +1,17 @@
+<table border="1">
+	<tr>
+		<td>Nama Dua </td>
+		<td>Kelas Dua </td>
+		<td>Jurusan Dua </td>
+	</tr>
+	<tr>
+		<td>Asad</td>
+		<td>12F</td>
+		<td>MI</td>
+	</tr>
+</table>
+
+<input type="text">
+<table>
+	
+</table>
